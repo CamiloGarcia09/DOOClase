d14c5963bb90da60790985abe302cf75c1d4d147
Camilo Garcia <juan092005ca@gmail.com>
2024-05-26 19:38:48 -0500

    MessageCatalogExternalService Finalizacion de la capa MessageCatalogExternalService, se agregaron las metodos necesarios para poder realizar la creacion de mensajes personalizados, que no son base en el sistema.

--- src/main/java/co/edu/uco/pch/crosscutting/exceptions/messagecatalog/impl/MessageCatalogExternalService.java
+++ src/main/java/co/edu/uco/pch/crosscutting/exceptions/messagecatalog/impl/MessageCatalogExternalService.java
@@ -1,11 +1,18 @@
 package co.edu.uco.pch.crosscutting.exceptions.messagecatalog.impl;
 
+import co.edu.uco.pch.crosscutting.exceptions.custom.CrosscuttingPCHException;
 import co.edu.uco.pch.crosscutting.exceptions.messagecatalog.MessageCatalog;
+import co.edu.uco.pch.crosscutting.exceptions.messagecatalog.MessageCatalogStrategy;
 import co.edu.uco.pch.crosscutting.exceptions.messagecatalog.data.CodigoMensaje;
 import co.edu.uco.pch.crosscutting.exceptions.messagecatalog.data.Mensaje;
+import co.edu.uco.pch.crosscutting.helpers.ObjectHelper;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public final class MessageCatalogExternalService implements MessageCatalog {
 
+    private final Map<String, Mensaje> mensajes = new HashMap<>();
 
     @Override
     public final void inicializar() {
@@ -14,11 +21,32 @@ public final class MessageCatalogExternalService implements MessageCatalog {
 
     @Override
     public final String obtenerCondidoMensaje(final CodigoMensaje codigo, final String... parametros) {
-        return "";
+        return obtenerMensaje(codigo, parametros).getContenido();
     }
 
     @Override
     public final Mensaje obtenerMensaje(final CodigoMensaje codigo, final String... parametros) {
-        return null;
+
+        if (ObjectHelper.getObjectHelper().isNull(codigo)) {
+            var mensajeUsuario = MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00002);
+            var mensajeTecnico = MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00001);
+            throw new CrosscuttingPCHException(mensajeTecnico, mensajeUsuario);
+        }
+
+        if (codigo.isBase()) {
+            var mensajeUsuario = MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00002);
+            var mensajeTecnico = MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00005, codigo.getIdentificador());
+            throw new CrosscuttingPCHException(mensajeTecnico, mensajeUsuario);
+        }
+
+        if (!mensajes.containsKey(codigo.getIdentificador())) {
+            var mensajeUsuario = MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00002);
+            var mensajeTecnico = MessageCatalogStrategy.getContenidoMensaje(CodigoMensaje.M00006, codigo.getIdentificador());
+            throw new CrosscuttingPCHException(mensajeTecnico, mensajeUsuario);
+        }
+
+        return mensajes.get(codigo.getIdentificador());
     }
+
 }
+
